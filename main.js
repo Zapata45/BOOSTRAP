a48931fa2b709fda618afb5b47451aa2255f72d5
Zapata45 <ankelijcole@gmail.com>
2025-01-08 15:30:30 +0200

    Version 1

---
+++ main.js
@@ -0,0 +1,13 @@
+// calculate the numbers in an array
+
+// const num=[1,2,3,4,5]
+
+// let sum = 0;
+// for (let i = 0; i < num.length; i++
+// ){sum + num [i];
+// console.log("total",sum)}
+let numbers[12,34,5,67]
+for (i = 0; i<numbers.length;i++){
+    if(numbers [i]= largest){largest=number}
+}
+console.log(largest)
